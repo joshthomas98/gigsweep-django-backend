f43da333b52dd9093ab169828a14055630b4e39c
joshthomas98 <joshua.thomas98@hotmail.co.uk>
2023-11-05 20:10:46 +0000

    Added gunicorn config after 'health: severe' warning

--- gigsweep_django_backend/wsgi.py
+++ gigsweep_django_backend/wsgi.py
@@ -11,6 +11,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gigsweep_django_backend.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE',
+                      'gigsweep_django_backend.settings')
 
 application = get_wsgi_application()
