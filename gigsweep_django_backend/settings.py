2947d2654d72ac53d7f1587c2936de4ada23b30e
joshthomas98 <joshua.thomas98@hotmail.co.uk>
2023-11-09 02:25:34 +0000

    Reverting back to .env way

--- gigsweep_django_backend/settings.py
+++ gigsweep_django_backend/settings.py
@@ -11,7 +11,11 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
 import os
+
+# Load environment variables from the .env file
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,12 +25,22 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure--i$udv*(n(3jq3feij+9sa0a&-jd^54nu%cq3m))+bp)cdr=!!'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+# Read the environment setting
+ENVIRONMENT = os.getenv('ENVIRONMENT')
 
-ALLOWED_HOSTS = ["*"]
+# DEBUG setting
+if ENVIRONMENT == 'development':
+    DEBUG = True
+else:
+    DEBUG = False
+
+# ALLOWED_HOSTS setting
+if ENVIRONMENT == 'development':
+    ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+else:
+    ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS').split(',')
 
 
 # Application definition
@@ -111,13 +125,12 @@ WSGI_APPLICATION = 'gigsweep_django_backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'gigsweep_schema',
-        'USER': 'admin',
-        'PASSWORD': 'Slash2406',
-        # or the MySQL server's IP address
-        'HOST': 'gigsweepdb.cuuwta3esqvt.eu-west-2.rds.amazonaws.com',
-        'PORT': '3306',       # the default MySQL port
+        'ENGINE': os.getenv('DB_ENGINE'),
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT'),
     }
 }
 
